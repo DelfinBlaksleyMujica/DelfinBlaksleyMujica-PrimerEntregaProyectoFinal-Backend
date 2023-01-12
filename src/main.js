818fea2a5f0c68cac7daf39ff9c605a206425d97
delfinblaksleymujica <delf.blaksley@hotmail.com>
2023-01-12 01:01:51 -0300

    Puerto hecho para aplicacion en Glitch

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 const app = require("./server.js")
 
-const PORT = 8080
+const PORT = process.env.PORT || 8080
 
 const server = app.listen( PORT , () => {
     console.log(`Servidor http escuchando en el puerto ${server.address().port}`);
